ddc5e84ed8ffa3f79b440f460d9ffb421224614d
pedrovalias <pedro.valias@hotmail.com>
2022-02-19 14:50:38 -0300

    #1 Commit de backup da refatoração - não tenho certeza se está funcionando corretamente

--- src/BanheiroGatos.cpp
+++ src/BanheiroGatos.cpp
@@ -2,12 +2,6 @@
 
 #include <NewPing.h>
 
-// #define ECHO_PIN_1      5    // Arduino pin tied to echo pin on the ultrasonic sensor.
-// #define TRIGGER_PIN_1   6    // Arduino pin tied to trigger pin on the ultrasonic sensor.
-
-//#define ECHO_PIN_2    7       // Arduino pin tied to echo pin on the ultrasonic sensor.
-//#define TRIGGER_PIN_2 8       // Arduino pin tied to trigger pin on the ultrasonic sensor.
-
 #define MAX_DISTANCE    55      // Distância máxima que queremos o ping(em centimetros). O máximo possível é 400-500cm.
 
 #define SONAR_NUM 2             // Número de sensores
@@ -24,20 +18,16 @@ NewPing sonar[SONAR_NUM] = {    // Sensor object array.
 #define LED_AZUL     3
 #define LED_VERMELHO 4
 
+#include <inicializar_pinagem.h>
+#include <inicializar_funcoes.h>
+#include <verificar_banheiro.h>
+
 int distancia = 0;
 int delayLoop = 3;
 const int limite_distancia = 32;
 boolean gato_usou_banheiro = false;
 int tempoEsperaSegundos = 3;    // Tempo determinado para espera é de 120 segundos   
 
-// int execucoesLimpeza = 0;
-
-void inicializaPinagem();
-void verifica_banheiro(int numSensor);
-void verificaStatusSistema();
-void executaLimpeza(int banheiro);
-void apagaLEDS();
-void piscaLEDS();
 
 void setup() {
 
@@ -68,63 +58,6 @@ void loop() {
   delay(delayLoop * 1000);
 }
 
-void inicializaPinagem(){
-  pinMode(PINO_VALVULA_1, OUTPUT);
-  digitalWrite(PINO_VALVULA_1, HIGH);
-
-  pinMode(PINO_VALVULA_2, OUTPUT);
-  digitalWrite(PINO_VALVULA_2, HIGH);
-
-  pinMode(LED_VERDE, OUTPUT);
-  digitalWrite(LED_VERDE, LOW);
-
-  pinMode(LED_VERMELHO, OUTPUT);
-  digitalWrite(LED_VERMELHO, LOW);
-
-  pinMode(LED_AZUL, OUTPUT);
-  digitalWrite(LED_AZUL, LOW);
-}
-
-void verifica_banheiro(int numSensor){
-   
-  distancia = validarDistancia(numSensor);
-
-  boolean gatoUsandoBanheiro = false ;
-
-  if(distancia <= limite_distancia && distancia != 0){
-    gatoUsandoBanheiro = true;
-    gato_usou_banheiro = true;
-
-    while(gatoUsandoBanheiro) {
-
-      Serial.println("O Gato está no banheiro");
-      
-      digitalWrite(LED_VERDE, LOW);
-      digitalWrite(LED_VERMELHO, HIGH);
-
-      distancia = validarDistancia(numSensor);
-
-      if(distancia > limite_distancia || distancia == 0) {
-        Serial.println("Banheiro liberado");
-        Serial.print("Tempo de espera antes de ligar a valvula: ");
-        for(int i = 0 ; i < tempoEsperaSegundos; i++){
-          delay(1000);
-          Serial.print(i);
-        }
-        // delay(5000);
-        gatoUsandoBanheiro = false;
-        digitalWrite(LED_VERMELHO, LOW);
-      }
-    }
-  }
-
-  Serial.print("Gato usou o banheiro "); 
-  Serial.print(numSensor);
-  Serial.print(" : ");
-  Serial.println(gato_usou_banheiro);
-
-  executaLimpeza(numSensor);
-}
 
 int validarDistancia(int dv){
   
---
+++ src/inicializar_funcoes.h
@@ -0,0 +1,7 @@
+void inicializaPinagem();
+void verifica_banheiro(int numSensor);
+int validarDistancia(int dv);
+void verificaStatusSistema();
+void executaLimpeza(int banheiro);
+void apagaLEDS();
+void piscaLEDS();
